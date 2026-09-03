15fedd2e59c3217c4112cbf4939e9cbbc1fdff75
tolgacakan14 <142435617+tolgacakan14@users.noreply.github.com>
2026-09-03 21:26:16 +0300

    Reorganize menu into barista categories

--- data/menu.ts
+++ data/menu.ts
@@ -22,40 +22,40 @@ const products = (items: Array<[string, number]>): Product[] => items.map(([name
 
 // 25 Ağustos 2026 tarihinde Franco'nun QRall menüsünden aktarıldı.
 export const menu: Category[] = [
+  { id: "tatlilar", name: "Tatlılar", eyebrow: "Tatlı bir mola", icon: "TL", products: products([
+    ["Dondurmalı Brownie",400],["Belçika Çikolatalı Brownie",400],["San Sabastian",360],
+    ["Belçika Çikolatalı Profiretol",400],["Dubai Cup",360],["Nutella Cookies",360],
+    ["Spoonful Cup",350],["Tiramisu",350],["Franco Snickers",350],["Çilekli Magnolia",350],["Lotus Küre",360]
+  ])},
+  { id: "soguk-kahveler", name: "ICED ESPRESSO BAR", eyebrow: "Buz gibi espresso", icon: "IE", products: products([
+    ["Ice Latte",220],["Ice Americano",220],["Ice Caramel Latte",240],["Ice Vanilla Latte",240],
+    ["Ice White Chocolate Mocha",240],["Ice Mocha",240],["Ice Toffee Nut Latte",240],["Frappe",230]
+  ])},
   { id: "dondurma", name: "Dondurma", eyebrow: "Franco gelato", icon: "GL", products: products([
     ["Sütlü",80],["Kakao",80],["Karamel",80],["Tahin",80],["İtalyan Karameli",80],["Bal Badem",80],
     ["Vişne",80],["Çilek",80],["Oreo",80],["Kavun",80],["Limon",80],["Yeşil Elma",80],
-    ["İncir&Ceviz",80],["Lotus",80],["Böğürtlen",80],["Antep Fıstığı",80],
-    ["Nutella",80],["Belçika Çikolatası",80]
+    ["İncir&Ceviz",80],["Lotus",80],["Böğürtlen",80],["Antep Fıstığı",80],["Nutella",80],["Belçika Çikolatası",80]
   ])},
-  { id: "soguk-kahveler", name: "Soğuk Kahveler", eyebrow: "Buz gibi kahve", icon: "SK", products: products([
-    ["Affagato",230],["Ice Latte",220],["Ice Vanilla Latte",240],["Ice Caramel Latte",240],
-    ["Ice Toffee Nut Latte",240],["Ice Americano",220],["Ice Filtre Kahve",170],["Ice Mocha",240],
-    ["Ice White Chocolate Mocha",240],["Frappe",230]
-  ])},
-  { id: "soguk-icecekler", name: "Soğuk İçecekler", eyebrow: "Serinleten tatlar", icon: "Sİ", products: products([
-    ["Su",60],["Sade Soda",80],["Sıkma Limon Soda",100],["Churchill",100],["Redbull",170],
-    ["Avşar",110],["Fanta",110],["Sprite",110],["Cola",110]
-  ])},
-  { id: "summer-edition", name: "Summer Edition", eyebrow: "Yaza özel", icon: "SE", products: products([
+  { id: "summer-edition", name: "Kokteyl", eyebrow: "Franco imza içecekleri", icon: "KT", products: products([
     ["Franco Mango",250],["Rooibos Peach",250]
   ])},
-  { id: "sicak-kahveler", name: "Sıcak Kahveler", eyebrow: "Taze hazırlanır", icon: "KK", products: products([
-    ["Double Türk Kahvesi",225],["Türk Kahvesi",150],["Latte",210],["Vanilla Latte",220],
-    ["Caramel Latte",220],["Toffienut Latte",220],["Americano",180],["Filtre Kahve",170],
-    ["Espresso",130],["Double Espresso",150],["Mocha",220],["White Chocolate Mocha",220],
-    ["Cortado",180],["Flat White",190],["Cappuccino",210]
+  { id: "sicak-kahveler", name: "ESPRESSO BAR", eyebrow: "Espresso temelli kahveler", icon: "EB", products: products([
+    ["Espresso",130],["Double Espresso",150],["Americano",180],["Latte",210],["Flat White",190],
+    ["Cappuccino",210],["Cortado",180],["Vanilla Latte",220],["Caramel Latte",220],
+    ["Toffienut Latte",220],["Mocha",220],["White Chocolate Mocha",220],["Affagato",230]
   ])},
-  { id: "sicak-icecekler", name: "Sıcak İçecekler", eyebrow: "Sıcacık bir mola", icon: "SÇ", products: products([
-    ["Fincan Çay",100],["Çay",70]
+  { id: "brew-bar", name: "Brew Bar", eyebrow: "Demleme kahveler", icon: "BR", products: products([
+    ["Türk Kahvesi",150],["Double Türk Kahvesi",225],["Filtre Kahve",170],["Ice Filtre Kahve",170]
   ])},
-  { id: "dondurmali-milkshake", name: "Dondurmalı Milkshake", eyebrow: "Yoğun ve serin", icon: "MS", products: products([
-    ["Lotus Milkshake",250],["Antepfıstıklı Milkshake",250],["Oreo & Çikolata Milkshake",250],["Çilek & Vanilya Milkshake",250]
+  { id: "dondurmali-milkshake", name: "Milkshake", eyebrow: "Yoğun ve serin", icon: "MS", products: products([
+    ["Çilek & Vanilya Milkshake",250],["Oreo & Çikolata Milkshake",250],
+    ["Antepfıstıklı Milkshake",250],["Lotus Milkshake",250]
   ])},
-  { id: "tatlilar", name: "Tatlılar", eyebrow: "Tatlı bir mola", icon: "TL", products: products([
-    ["Dondurmalı Brownie",400],["Belçika Çikolatalı Brownie",400],["San Sabastian",360],
-    ["Belçika Çikolatalı Profiretol",400],["Dubai Cup",360],
-    ["Nutella Cookies",360],["Spoonful Cup",350],["Tiramisu",350],["Franco Snickers",350],
-    ["Çilekli Magnolia",350],["Lotus Küre",360]
+  { id: "soguk-icecekler", name: "Soft", eyebrow: "Soğuk içecekler", icon: "SF", products: products([
+    ["Cola",110],["Sprite",110],["Fanta",110],["Avşar",110],["Redbull",170],
+    ["Su",60],["Sade Soda",80],["Sıkma Limon Soda",100],["Churchill",100]
+  ])},
+  { id: "sicak-icecekler", name: "Hot", eyebrow: "Sıcak içecekler", icon: "HT", products: products([
+    ["Çay",70],["Fincan Çay",100]
   ])}
 ];
